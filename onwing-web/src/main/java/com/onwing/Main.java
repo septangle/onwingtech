69a9f50617b72744b06402ef62bfb2483eff31f7
yuxiaoyang <you@example.com>
2017-12-18 17:59:35 +0800

    add embed tomcat

---
+++ onwing-web/src/main/java/com/onwing/Main.java
@@ -0,0 +1,43 @@
+package com.onwing;
+
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+public class Main {
+	private final static String webappDirLocation = "src/main/webapp/";
+	private static Tomcat tomcat = null;
+
+	public static void main(String[] args) {
+
+		tomcat = new Tomcat();
+		tomcat.setPort(8080);
+
+		try {
+			StandardContext ctx = (StandardContext) tomcat.addWebapp("/onwing-web",
+					new File(webappDirLocation).getAbsolutePath());
+			System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+			// Declare an alternative location for your "WEB-INF/classes" dir
+			// Servlet 3.0 annotation will work
+			/*
+			File additionWebInfClasses = new File("target/classes");
+			WebResourceRoot resources = new StandardRoot(ctx);
+			resources.addPreResources(
+					new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+			ctx.setResources(resources);
+			*/
+			tomcat.start();
+			tomcat.getServer().await();
+		} catch (Exception e) {
+			System.out.println("Tomcat could not be started.");
+			e.printStackTrace();
+		}
+	}
+}
