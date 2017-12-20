7837ba4ffb2c3f1213afa368140ee9fe08c32b26
zhouchuang1709 <kyd_x1709@163.com>
2017-12-20 11:06:03 +0800

    edit

--- onwing-html/app/scripts/main.js
+++ onwing-html/app/scripts/main.js
@@ -1,1 +1,20 @@
 console.log('\'Allo \'Allo!');
+
+window.onwing = (function (onwing, $, undefined) {
+
+  var validateRegx = {
+    tel: ['^0\\d{2,3}\\-?\\d{7,8}$','电话号码格式错误'],
+    mobile: ['^1\\d{10}$','手机号码格式错误'],
+    password: ['^[\\w\!\@\#\$\%\^\&\(\)\*]{6,}$','密码格式错误'],
+    username: ['^[\\w\u4e00-\u9fa5]{2,}$','用户名格式错误']
+  }
+  onwing.validate = function(validateRegxArr){
+    var reg = new RegExp(validateRegx.username[0],'g');
+    var test = '周闯'
+    return console.info(reg.test(test));
+  }
+  return onwing;
+})(window.onwing || {}, jQuery);
+
+onwing.validate();
+
