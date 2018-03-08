5671e36ffd37b2cd5b714afd06c512bf5301f0f5
zx <zx@ungrui.com>
2018-03-08 18:16:14 +0800

    add community entity

---
+++ onwing-comm-dal/src/main/java/com/onwing/household/comm/dal/dao/CommunityMapper.java
@@ -0,0 +1,10 @@
+package com.onwing.household.comm.dal.dao;
+
+import com.onwing.household.comm.dal.model.Community;
+
+public interface CommunityMapper {
+	
+	Community selectByPrimaryKey(Long id);
+
+
+}
